6fe19f29a631b574cd5f7ea2d6d09a477f247c56
unknown <kenny.hom@bchainco.com>
2018-08-13 15:30:58 -0400

    finished server auth

--- models/user.js
+++ models/user.js
@@ -1,11 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 // Define model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
 });
+
+// On Save Hook, encrypt password
+// Before saving a model, run function
+userSchema.pre("save", function(next) {
+  const user = this;
+
+  // Generate salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // Encrypt our password with salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      // Overwrite plain test password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
 
 // Create model class
 const ModelClass = mongoose.model("user", userSchema);
