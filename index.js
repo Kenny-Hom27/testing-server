859fda567d5912073b906ca2ad68b940e69405b7
Kenny <kenny.hom@bchainco.com>
2018-08-23 16:58:51 -0400

    added cors

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
-const router = require('./router');
-const mongoose = require('mongoose');
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // DB Setup
-mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb://localhost:27017/auth",
+  { useNewUrlParser: true }
+);
 
 // App Setup
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(morgan("combined"));
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
-const server = http.createServer(app)
+const server = http.createServer(app);
 server.listen(port);
-console.log('Server listneing on:', port)
+console.log("Server listneing on:", port);
